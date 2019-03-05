d3b418c683365c556b52dd88e7d7f12220a658f6
Enaxom <enaxom@gmail.com>
2019-03-05 11:57:04 +0100

    New activities created (all questions types). Associated XML created.

--- app/src/main/java/com/morgane/quizit/MainActivity.java
+++ app/src/main/java/com/morgane/quizit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.morgane.quizit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
 
         MultipleQuestion test = new MultipleQuestion(Themes.TECH, "Quelle est la réponse", "La réponse", "Wrong", "Wrong", "Wrong");
     }
+
+    public void playAll(View view) {
+        Intent k = new Intent(MainActivity.this, MultipleQuestionActivity.class);
+        startActivity(k);
+    }
+
+    public void playTech(View view) {
+        Intent k = new Intent(MainActivity.this, TrueFalseActivity.class);
+        startActivity(k);
+    }
+
+    public void playCulture(View view) {
+        Intent k = new Intent(MainActivity.this, ImageQuestionActivity.class);
+        startActivity(k);
+    }
+
+    public void playAnimal(View view) {
+        return;
+    }
+
+    public void playScience(View view) {
+        return;
+    }
+
 }
---
+++ app/src/main/java/com/morgane/quizit/TrueFalseActivity.java
@@ -0,0 +1,41 @@
+package com.morgane.quizit;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+public class TrueFalseActivity extends AppCompatActivity {
+
+    ProgressBar progress;
+    MyCountDownTimer countTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_true_false);
+
+        progress = findViewById(R.id.progressbar);
+        countTimer = new MyCountDownTimer(10000, 100);
+        countTimer.start();
+    }
+
+    public class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            int progressTime = (int) (millisUntilFinished/100);
+            progress.setProgress(progress.getMax() - progressTime);
+        }
+
+        @Override
+        public void onFinish() {
+            finish();
+        }
+    }
+
+}
