333186f4a014d43259157dfa564c3196f1c7acba
Enaxom <enaxom@gmail.com>
2019-03-06 17:48:09 +0100

    CSV files with questions and images added. TODO: get datas.

--- app/src/main/java/com/morgane/quizit/MainActivity.java
+++ app/src/main/java/com/morgane/quizit/MainActivity.java
@@ -7,12 +7,22 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Find a way to make the lists
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MultipleQuestion test = new MultipleQuestion(Themes.TECH, "Quelle est la réponse", "La réponse", "Wrong", "Wrong", "Wrong");
+
+    }
+
+    public void readFill(String filename) {
+        // multiple_questions.csv -> 6 columns : THEME ; question ; answer ; wrong ; wrong ; wrong
+        // true_false_questions.csv -> 3 columns : THEME ; question ; answer (0/1)
+        // image_question.csv -> 7 columns : THEME ; question ; drawable ; answer ; wrong ; wrong ; wrong
+
+
     }
 
     public void playAll(View view) {
